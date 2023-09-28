ec2f77649a57176a2768488d0384f4390c677f8b
YasminRez <142317229+YasminRez@users.noreply.github.com>
2023-09-28 14:38:46 -0300

    Update exercio2.c

--- exercio2.c
+++ exercio2.c
@@ -2,16 +2,14 @@
 
 int main() {
     long int numero;
-     printf("digite um numero entre 0 e 1 bilhao: \n");
+
      scanf("%d", &numero);
 
-     if(numero <0 || numero > 1000000000){
-        printf("invalido insira um número entre 0 e 1000000000.\n"); 
-       
-     } else if(numero % 2 == 0) {
-        printf("%d eh par.\n", numero);
+     
+       if(numero % 2 == 0) {
+        printf("%d eh par\n", numero);
      } else if (numero % 2 != 0){
-            printf("%d eh impar.\n", numero);
+            printf("%d eh impar\n", numero);
         }
      
      long int numero2 = numero;
@@ -21,7 +19,7 @@ int main() {
         soma += numero2% 10;
         numero2 /= 10;
        } 
-       printf("A soma dos algarismo %d eh igual %d.\n", numero, soma);
+       printf("A soma dos algarismo %d eh igual %d\n", numero, soma);
 
     return 0;
 }
