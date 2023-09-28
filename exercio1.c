b404c026722f95e1d29791163c1478ab5dd7ccf4
YasminRez <142317229+YasminRez@users.noreply.github.com>
2023-09-28 14:37:42 -0300

    Update exercio1.c

--- exercio1.c
+++ exercio1.c
@@ -1,32 +1,69 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+
     int ano;
-    int olim = 4;
-    int copa = 4;
-    // Pedir o ano de entrada
-    printf("digite um ano: ");
+
     scanf("%d", &ano);
 
-    if (ano < 1800 || ano > 2022) {
-        printf("Ano invalido. Digite um ano entre 1800 e 2022.\n");
-    } else {
-        if (ano >= 1939 && ano <= 1946) {
-            printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
-        } else {
 
-            if ((ano - 1896) % olim == 0 || (ano - 1930) % copa == 0) {
-                if ((ano - 1896) % olim == 0 && (ano - 1930) % copa == 0) {
-                    printf("houve Jogos Olimpicos de Verao e Copa do Mundo no ano de %d.\n", ano);
-                } else if ((ano - 1896) % olim == 0) {
-                    printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano);
-                } else {
-                    printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano);
-                }
-            } else {
-                printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de  %d.\n", ano);
-            }
-        }
+    switch(ano){
+        //olim inicio
+        case 1896: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+         case 1900: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+          case 1904: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+           case 1908: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+            case 1912: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+             case 1920: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+              case 1924: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                 case 1928: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                  case 1932: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                    case 1936: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                      case 1948: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                        case 1952: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                      case 1956: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                     case 1960: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                   case 1964: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+                case 1968: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+              case 1972: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+           case 1976: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+          case 1980: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+         case 1984: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 1988: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 1992: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 1996: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2000: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2004: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2008: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2012: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2016: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+        case 2020: printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n", ano); break;
+// fim olim
+        case 1930: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1934: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1938: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1950: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1954: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1958: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1962: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1966: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1970: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1974: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1978: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1982: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1986: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1990: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1994: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 1998: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2002: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2006: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2010: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2014: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2018: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+        case 2022: printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n", ano); break;
+
+        default:printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n", ano);
     }
+
     return 0;
 }
